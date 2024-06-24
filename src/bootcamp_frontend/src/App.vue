f41c568bd86a36e76ae30bde8436becfd3d735f7
Pawelo <golata.pawel06@gmail.com>
2024-06-24 19:09:17 +0200

    Added counter and mini styles

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -1,14 +1,33 @@
 <template lang="html">
   <div>
     <img src="/logo2.svg" alt="DFINITY logo" />
-    <h2>Cześć</h2>
+    <h2>Cześć!</h2>
+    <button @click="increase">Zwiększ licznik</button>
+    <p>Licznik: {{ licznik }}</p>
   </div>
 </template>
 <script>
 export default {
-  
+  data(){
+    return{
+      licznik: 0
+    }
+  },
+  methods: {
+    increase(){
+      this.licznik++
+    }
+  }
 }
 </script>
 <style lang="css">
-  
+  h2{
+    color: #5d2684;
+  }
+  button{
+    border-radius: .4rem;
+    border: 2px solid #5d2684;
+    background: none;
+    padding: 1rem 2em;
+  }
 </style>
