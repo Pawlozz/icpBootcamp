52346fb5a79c29c1c69dd3c043822f30a9dea5c3
Pawelo <golata.pawel06@gmail.com>
2024-06-24 18:37:13 +0200

    first commit

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    <h2>Cześć</h2>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="css">
+  
+</style>
